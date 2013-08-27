006b85cbf546859cddec1d34d51b841ae4f7501a
Alexandra Swart <answart@sbcglobal.net>
2013-08-27 00:37:29 -0700

    Make a basic User model (including secure passwords)

---
+++ db/migrate/20130827054814_add_index_to_users_email.rb
@@ -0,0 +1,11 @@
+class AddIndexToUsersEmail < ActiveRecord::Migration
+
+	# 6.19: migration for enforcing email uniqueness
+	# uses a Rails method called add_index to add an
+	# index on the email column of the users table.
+	#  The index by itself doesn’t enforce uniqueness, 
+	# but the option unique: true does.
+  def change
+    add_index :users, :email, unique: true
+  end
+end

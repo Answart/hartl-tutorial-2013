863322c770b68f74b63dbeb8ebc793d98dd6062a
Alexandra Swart <answart@sbcglobal.net>
2013-08-26 20:09:33 -0700

    Finish 5.6 Exercises

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,7 @@
-# to replace the 'let(:base_title) { "Ruby on Rails Tutorial Sample App" }' in
-# static_pages_spec.rb
+include ApplicationHelper
+
+# to replace the 'let(:base_title) { "Ruby on Rails Tutorial Sample
+# App" }' in static_pages_spec.rb
 
 def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
